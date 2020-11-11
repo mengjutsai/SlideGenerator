918b4878a2af423e2956139afa6bfb0dba1b5acb
Meng-Ju Tsai <meng-ju.tsai@cern.ch>
2020-11-11 20:15:52 +0100

    update script to run

--- generation_script.sh
+++ generation_script.sh
@@ -2,3 +2,9 @@ python generateSlides.py  --input slides/slides_20201109_object_mig/generation-S
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR.py --output slides/slides_20201109_object_mig/ --outputfile "SR_v1.tex" --inputFolder "/Users/ploww/dropbox_meng/HEP/Results/BSM4tops/Slides20201106_new_offline_production/results/" --compile
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j.py --output slides/slides_20201109_object_mig/ --outputfile "SR_6j_v1.tex" --inputFolder "/Users/ploww/dropbox_meng/HEP/Results/BSM4tops/Slides20201106_new_offline_production/results/" --compile
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j_2b.py --output slides/slides_20201109_object_mig/ --outputfile "SR_6j_2b_v1.tex" --inputFolder "/Users/ploww/dropbox_meng/HEP/Results/BSM4tops/Slides20201106_new_offline_production/results/" --compile
+
+
+python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SS3L1b.py --output slides/slides_20201109_object_mig/ --outputfile "SS3L1b_v1.tex" --inputFolder "../hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
+python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR.py --output slides/slides_20201109_object_mig/ --outputfile "SR_v1.tex" --inputFolder "../hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
+python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j.py --output slides/slides_20201109_object_mig/ --outputfile "SR_6j_v1.tex" --inputFolder "../hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
+python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j_2b.py --output slides/slides_20201109_object_mig/ --outputfile "SR_6j_2b_v1.tex" --inputFolder "../hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
