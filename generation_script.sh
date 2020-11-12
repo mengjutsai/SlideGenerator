b1ca98e3eab336979154241e4e83ee984762aefb
Meng-Ju Tsai <meng-ju.tsai@cern.ch>
2020-11-12 18:17:31 +0100

    update

--- generation_script.sh
+++ generation_script.sh
@@ -8,3 +8,10 @@ python generateSlides.py  --input slides/slides_20201109_object_mig/generation-S
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR.py --output slides/slides_20201109_object_mig/ --outputfile "SR_v1.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j.py --output slides/slides_20201109_object_mig/ --outputfile "SR_6j_v1.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j_2b.py --output slides/slides_20201109_object_mig/ --outputfile "SR_6j_2b_v1.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
+
+
+
+python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SS3L1b.py --output /lustre/umt3/user/metsai/slides/20201109_object_migration/ --outputfile "SS3L1b.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
+python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR.py --output /lustre/umt3/user/metsai/slides/20201109_object_migration/ --outputfile "SR.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
+python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j.py --output /lustre/umt3/user/metsai/slides/20201109_object_migration/ --outputfile "SR_6j.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
+python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j_2b.py --output /lustre/umt3/user/metsai/slides/20201109_object_migration/ --outputfile "SR_6j_2b.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
