f9ab70fd5c14f40e90e66a1daf7a91336160aa34
Meng-Ju Tsai <meng-ju.tsai@cern.ch>
2020-11-27 23:52:13 +0100

    update

--- slides/slides_20201127_pBDT/generation-pBDT.py
+++ slides/slides_20201127_pBDT/generation-pBDT.py
@@ -3,8 +3,8 @@ from dictionary import *
 import glob
 
 def plot(outF,subfolder,inputFolder=""):
-    # outF.write('\\begin{center}{'+subfolder.split("/")[-3].replace('_','\\')+'}\\end{center}\n')
-    # outF.write('\\begin{center}{'+subfolder.split("/")[-2].replace('_','\\')+'}\\end{center}\n')
+    outF.write('\\begin{center}\n{'+subfolder.split("/")[-3].replace('_','\\')+'}\n \\end{center}\n')
+    outF.write('\\begin{center}\n{'+subfolder.split("/")[-2].replace('_','\\')+'}\n \\end{center}\n')
 
     list = glob.glob(inputFolder+subfolder+'*')
 
