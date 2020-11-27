f14482ec49048d88d8a2ec3f85131bd2f77b0d86
Meng-Ju Tsai <meng-ju.tsai@cern.ch>
2020-11-27 23:38:43 +0100

    update

--- slides/slides_20201127_pBDT/generation-pBDT.py
+++ slides/slides_20201127_pBDT/generation-pBDT.py
@@ -3,9 +3,9 @@ from dictionary import *
 import glob
 
 def plot(outF,subfolder,inputFolder=""):
-    outF.write('\\begin{center}{'+subfolder.split("\\")[-2]+'}\\end{center}\n')
-    # outF.write('\\Section{'+subfolder.split("\\")[-2]+'}\n')
-    outF.write('\\begin{center}{'+subfolder.split("\\")[-1]+'}\\end{center}\n')
+    outF.write('\\begin{center}{'+subfolder.split("/")[-2]+'}\\end{center}\n')
+    # outF.write('\\Section{'+subfolder.split("/")[-2]+'}\n')
+    outF.write('\\begin{center}{'+subfolder.split("/")[-1]+'}\\end{center}\n')
     dict = glob.glob(inputFolder+subfolder)
 
     fileList = []
@@ -18,7 +18,7 @@ def plot(outF,subfolder,inputFolder=""):
     remain = len(fileList) % 3
 
     for ifile, file in enumerate(fileList):
-        filename = file.split("\\")[-1]
+        filename = file.split("/")[-1]
         caption = filename
         caption = caption.replace('_','\\_')
 
