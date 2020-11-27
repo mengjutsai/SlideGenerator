2ca441c37b3f7ad0b9f99aeccaee3552dff55c3d
Meng-Ju Tsai <metsai@cern.ch>
2020-11-27 10:19:03 +0100

    update

--- generation_script.sh
+++ generation_script.sh
@@ -1,3 +1,9 @@
+
+# pBDT
+python generateSlides.py  --input slides/slides_20201127_pBDT/generation-pBDT.py --output /lustre/umt3/user/metsai/slides/slides_20201127_pBDT/ --outputfile "pBDT.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/hbsm4topssml_pbdt/results/pBDT/validation/" --compile
+
+
+# object migration
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SS3L1b.py --output /lustre/umt3/user/metsai/slides/20201109_object_migration/ --outputfile "SS3L1b_vjets.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR.py --output /lustre/umt3/user/metsai/slides/20201109_object_migration/ --outputfile "SR_vjets.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
 python generateSlides.py  --input slides/slides_20201109_object_mig/generation-SR_6j.py --output /lustre/umt3/user/metsai/slides/20201109_object_migration/ --outputfile "SR_6j_vjets.tex" --inputFolder "/atlas/data19/metsai/hbsm4top/bsm4topssml_fitter/bsm4top_plottool/object_migration/results/" --compile
---
+++ slides/slides_20201127_pBDT/generation-pBDT.py
@@ -0,0 +1,62 @@
+from bookingSlides import *
+from dictionary import *
+import glob
+
+def plot(outF,inputFolder=""):
+
+    dict = glob.glob("vali*/sta*")
+    # dir = "SR_6j_2b"
+    
+    fileList = []
+    for folder in dict:
+        fileList.append(folder+"/"+"stackplot_pBDT400_multiclass_cat_even_test_odd_allEvent.pdf")
+    print(fileList)
+                # if ',' in file:
+                #     suffix = file.split(',')[1]
+                # else:
+                #     suffix = ''
+                #
+                # if len(suffix)>0:
+                #     Title = "\large{"+sel+":"+file.split('.')[0]+":"+suffix+":"+campaign+"}"
+                #     VarList = [campaign+"_"+file.split('.')[0]+"_"+suffix+"_new_"+item+"_old_"+NewOld_var[item] for item in plot_dict[plot]]
+                # else:
+                #     Title = "\large{"+sel+":"+file.split('.')[0]+":"+campaign+"}"
+                #     VarList = [campaign+"_"+file.split('.')[0]+"__new_"+item+"_old_"+NewOld_var[item] for item in plot_dict[plot]]
+                #
+                #
+                # Title = Title.replace('_','\\_')
+                #
+                # # print(Title)
+                #
+                # if "6p" in plot:
+                #     make_6plots(outF, Title,VarList,inputFolder+sel+"/")
+                # elif "2p" in plot:
+                #     make_2plots(outF, Title,VarList,inputFolder+sel+"/")
+                # elif "3p" in plot:
+                #     make_3plots(outF, Title,VarList,inputFolder+sel+"/")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
